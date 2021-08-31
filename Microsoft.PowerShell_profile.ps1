8a376756f0c669e8bfb988c882da6b4d16ed9f1b
Phil Boyd <phil.boyd@prb-services-llc.com>
2021-08-30 20:10:21 -0400

    wip(docker): update PS functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -85,28 +85,43 @@ if (Test-Path($ChocolateyProfile))
 	Import-Module "$ChocolateyProfile"
 }
 
-function Windows-Docker
+function PRB-WindowsDocker
 {
 	Set-Location -Path "C:\Program Files\Docker\Docker"
 	.\DockerCli.exe -SwitchDaemon -SwitchWindowsEngine
 }
 
-function Linux-Docker
+function PRB-LinuxDocker
 {
 	Set-Location -Path "C:\Program Files\Docker\Docker"
 	.\DockerCli.exe -SwitchDaemon -SwitchLinuxEngine
 }
 
-function Load-Azurite
+function PRB-LoadAzurite
 {
 	Linux-Docker
 	docker run -d --rm -p 10000:10000 -p 10001:10001 -p 10002:10002 -v /e/data-azurite:/data -v ${pwd}:/workspace mcr.microsoft.com/azure-storage/azurite:latest
 }
 
-function Load-CosmoDB
+function PRB-LoadCosmoDB
 {
 	Windows-Docker
 	$bind_mount='e:\CosmosDBEmulator\bind-mount'
 	md "${bind_mount}" 2>null
-	docker run --name azure-cosmosdb-emulator --memory 2GB --mount "type=bind,source=${bind_mount},destination=C:\CosmosDB.Emulator\bind-mount" --interactive --tty -p 8081:8081 -p 8900:8900 -p 8901:8901 -p 8902:8902 -p 10250:10250 -p 10251:10251 -p 10252:10252 -p 10253:10253 -p 10254:10254 -p 10255:10255 -p 10256:10256 -p 10350:10350 mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
+	docker run -d --rm --name cosmosdb `
+		--memory 2GB `
+		--mount "type=bind,source=${bind_mount},destination=C:\CosmosDB.Emulator\bind-mount"  `
+		--tty `
+		-p 8081:8081 `
+		-p 8900:8900 `
+		-p 8901:8901 `
+		-p 8902:8902 `
+		-p 10250:10250 `
+		-p 10251:10251 `
+		-p 10252:10252 `
+		-p 10253:10253 `
+		-p 10254:10254 `
+		-p 10255:10255 `
+		-p 10256:10256 `
+		-p 10350:10350 mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
 }
