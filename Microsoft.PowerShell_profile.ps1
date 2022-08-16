f5314fb5e3564c6bdf522408249f9523d1a7d6fc
Phil Boyd <Phil.Boyd@csat.com>
2022-08-16 07:01:37 -0400

    task(ps): update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,98 +1,88 @@
-Import-Module posh-git
+oh-my-posh.exe init pwsh | Invoke-Expression
 
-function PRB-SetPanicReadOnly
+function vci
 {
-	$f = get-item e:\docker\images\panic.log
-	$f.Attributes = $f.Attributes -band (-bnot [System.IO.FileAttributes]::ReadOnly)
+    $param1 = $args[0]
+    code-insiders $param1
 }
 
-function PRB-WindowsDocker
+function restartDocker
 {
-	PRB-SetPanicReadOnly
-	$cwd = ${pwd}
-	Set-Location -Path "C:\Program Files\Docker\Docker\"
-	.\DockerCli.exe -SwitchWindowsEngine
-	Start-Sleep -s 5
-	Set-Location -Path ${cwd}
+    $processes = Get-Process '*docker desktop*'
+    if ($processes.Count -gt 0)
+    {
+        $processes[0].Kill()
+        $processes[0].WaitForExit()
+    }
+    Start-Process 'C:\Program Files\Docker\Docker\Docker Desktop.exe'
+}
+
+function checkDocker
+{
+    Set-Location $env:LOCALAPPDATA/Docker/wsl/
+    Get-ChildItem -Recurse
+}
+
+function Move-Docker-Data
+{
+    wsl --export docker-desktop-data 'd:/wsl/docker-desktop-data/data.tar'
+    wsl --unregister docker-desktop-data
+    wsl --import docker-desktop-data 'D:/wsl/docker-desktop-data' 'd:/wsl/docker-desktop-data/data.tar'
 }
 
 function PRB-LinuxDocker
 {
-	PRB-SetPanicReadOnly
-	$cwd = ${pwd}
-	Set-Location -Path "C:\Program Files\Docker\Docker\"
-	.\DockerCli.exe -SwitchLinuxEngine
-	Start-Sleep -s 5
-	Set-Location -Path ${cwd}
+    PRB-SetPanicReadOnly
+    $cwd = ${pwd}
+    Set-Location -Path 'C:\Program Files\Docker\Docker\'
+    .\DockerCli.exe -SwitchLinuxEngine
+    Start-Sleep -s 5
+    Set-Location -Path ${cwd}
 }
 
 function PRB-LoginGovAzure
 {
-	az cloud set --name AzureUSGovernment
-	az login
+    az cloud set --name AzureUSGovernment
+    az login
 }
 
 function PRB-LoginCommercialAzure
 {
-	az cloud set --name AzureCloud
-	az login
+    az cloud set --name AzureCloud
+    az login
 }
 
 function PRB-LoadAzurite
 {
-	PRB-LinuxDocker
-	docker run -d --rm -p 10000:10000 -p 10001:10001 -p 10002:10002 -v /e/data-azurite:/data -v ${pwd}:/workspace mcr.microsoft.com/azure-storage/azurite:latest
-}
-
-function PRB-LoadAzCli
-{
-	PRB-LinuxDocker
-	docker run -it --rm -v ${HOME}/.ssh:/root/.ssh -v ${pwd}:/workspace -w /workspace --name azure-cli mcr.microsoft.com/azure-cli bash 
-}
-
-function PRB-LoadAwsCliV1
-{
-	PRB-LinuxDocker
-	docker run -it --rm -v ${HOME}/.ssh:/home/node/.ssh -v ${HOME}/.aws:/home/node/.aws -v ${pwd}:/workspace -w /workspace --name aws-cli prbservicesllc/aws-cdk:v1
-}
-
-function PRB-LoadAwsCliV2
-{
-	PRB-LinuxDocker
-	docker run -it --rm -v ${HOME}/.ssh:/home/node/.ssh -v ${HOME}/.aws:/home/node/.aws -v ${pwd}:/workspace -w /workspace --name aws-cli prbservicesllc/aws-cdk:v2
-}
-
-function PRB-LoadTerraform
-{
-	PRB-LinuxDocker
-	# docker run -it --rm -v ${HOME}/.ssh:/root/.ssh -v ${HOME}/.aws:/root/.aws -v ${HOME}/.azure:/root/.azure -v ${pwd}:/workspace -w /workspace --name terraform prismea/terraform-cli-public:latest bash 
+    PRB-LinuxDocker
+    docker run -d --rm -p 10000:10000 -p 10001:10001 -p 10002:10002 -v /e/data-azurite:/data -v ${pwd}:/workspace mcr.microsoft.com/azure-storage/azurite:latest
 }
 
 function PRB-LoadCosmosDB
 {
-	PRB-WindowsDocker
-	$hostDirectory="$Env:RepoDir/CosmosDBEmulator/bind-mount"
-	Set-Location -Path "$Env:RepoDir"
-	md "${hostDirectory}" 2>null
-	Set-Location -Path "$Env:RepoDir/docker-sandbox/"
-	# docker compose -f ./docker-compose.cosmosdb.yml up -d
-	docker run -d --rm `
-		--name cosmosdb `
-		--memory 3GB `
-		--mount "type=bind,source=${hostDirectory},destination=C:\CosmosDB.Emulator\bind-mount"  `
-		--tty `
-		-p 8081:8081 `
-		-p 8900:8900 `
-		-p 8901:8901 `
-		-p 8902:8902 `
-		-p 10250:10250 `
-		-p 10251:10251 `
-		-p 10252:10252 `
-		-p 10253:10253 `
-		-p 10254:10254 `
-		-p 10255:10255 `
-		-p 10256:10256 `
-		-p 10350:10350 mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
-	Set-Location -Path ${hostDirectory}
-	./importcert.ps1
+    PRB-WindowsDocker
+    $hostDirectory = "$Env:RepoDir/CosmosDBEmulator/bind-mount"
+    Set-Location -Path "$Env:RepoDir"
+    md "${hostDirectory}" 2>null
+    Set-Location -Path "$Env:RepoDir/docker-sandbox/"
+    # docker compose -f ./docker-compose.cosmosdb.yml up -d
+    docker run -d --rm `
+        --name cosmosdb `
+        --memory 3GB `
+        --mount "type=bind,source=${hostDirectory},destination=C:\CosmosDB.Emulator\bind-mount"  `
+        --tty `
+        -p 8081:8081 `
+        -p 8900:8900 `
+        -p 8901:8901 `
+        -p 8902:8902 `
+        -p 10250:10250 `
+        -p 10251:10251 `
+        -p 10252:10252 `
+        -p 10253:10253 `
+        -p 10254:10254 `
+        -p 10255:10255 `
+        -p 10256:10256 `
+        -p 10350:10350 mcr.microsoft.com/cosmosdb/windows/azure-cosmos-emulator
+    Set-Location -Path ${hostDirectory}
+    ./importcert.ps1
 }
